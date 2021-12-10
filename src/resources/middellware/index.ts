fce109b5ea7b334d4823bbb63e351ca12cf8120f
Андрей <andrei7402@gmail.com>
2021-12-10 15:05:47 +0300

    Delete file

--- src/resources/middellware/index.ts
+++
@@ -1,5 +0,0 @@
-import {errorHandling} from '../middlewares/errorHandling';
-import { logging } from '../middlewares/logging';
-import { unhandledRejection, uncaughtException } from '../middlewares/uncaughtHandling';
-
-export { logging, errorHandling, unhandledRejection, uncaughtException };
